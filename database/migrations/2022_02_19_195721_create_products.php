336e4333574d614559c25352e6e5d5f896f01441
ilovemacross <renanmachadopinho@gmail.com>
2022-02-25 16:00:16 -0300

    main

--- database/migrations/2022_02_19_195721_create_products.php
+++ database/migrations/2022_02_19_195721_create_products.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table()->id();
+            $table->id();
             $table->string('name')->index();
             $table->string('price');
             $table->integer('user_id');
